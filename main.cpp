19e827ac0b50eae476c1272d47316131d29ce7f7
macorrea <ma-xlo@debian.core>
2022-06-29 17:44:22 -0300

    chore: improved input ui

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
-/*
-//1) Exibir o sistema;
-
-//2) Exibir o resultado do determinante;
-
-3) Prever e fazer teste para os possíveis erros (erro de digitação e outros);
-
-9) Exibir a solução do sistema utilizando a Regra de Cramer.
-
-*/
-
 #include <iostream>
 #include <string>
 #include <malloc.h>
@@ -20,7 +9,7 @@ void preencheSistema2 (float matriz[2][4], int opcao) {
     char coef[2] = {'x', 'y'}; 
 
     for(int i = 0; i < opcao + 1; i++) {
-        cout << "Expressão " << i+1 << endl;
+        cout << " +- Equação " << i+1 << " -----------------------+" << endl;
         for(int j = 0; j < opcao + 1; j++) {
             cout << "  Coeficiente da variável " << coef[j] << ": ";
             //_flushall();
@@ -57,9 +46,10 @@ void imprimeSistema2 (float matriz[2][4], int opcao)
     char coef[2] = {'x', 'y'}; 
     char tecla;
 
-    cout << endl << "Sistema inserido:" << endl;
+    cout << endl << " +-------- Sistema inserido ---------+" << endl;
 
     for(int i = 0; i < opcao + 1; i++) {
+        cout << "            ";
         for(int j = 0; j <= opcao + 1; j++) {
             if (matriz[i][j] < 0)
                 cout << "+ " << "(" << matriz[i][j] << coef[j] << ")" << " ";
@@ -79,7 +69,7 @@ void preencheSistema3 (float matriz[3][4], int opcao) {
     char coef[3] = {'x', 'y', 'z'}; 
 
     for(int i = 0; i < opcao + 1; i++) {
-        cout << "Expressão " << i+1 << endl;
+        cout << " +- Equação " << i+1 << " -----------------------+" << endl;
         for(int j = 0; j < opcao + 1; j++) {
             cout << "  Coeficiente da variável " << coef[j] << ": ";
             //_flushall();
@@ -89,6 +79,28 @@ void preencheSistema3 (float matriz[3][4], int opcao) {
                 cout << "  Resultado" << ": ";
                 cin >> matriz[i][3];
 
+            }
+        }
+    }
+}
+
+void imprimeSistema3 (float matriz[3][4], int opcao) 
+{
+    char coef[3] = {'x', 'y', 'z'}; 
+
+    cout << endl << " +-------- Sistema inserido ---------+" << endl;
+    for(int i = 0; i < opcao + 1; i++) {
+        cout << "         ";
+        for(int j = 0; j <= opcao + 1; j++) {
+            if (matriz[i][j] < 0)
+                cout << "+ " << "(" << matriz[i][j] << coef[j] << ")" << " ";
+            else 
+                cout << "+ " << matriz[i][j] << coef[j] << " ";
+
+
+            if( j == 2) {
+                cout << "= " << matriz[i][3] << endl;
+                break;
             }
         }
     }
@@ -124,33 +136,13 @@ float * determinante3(float matriz[3][4])
     return detGeral;
 }
 
-void imprimeSistema3 (float matriz[3][4], int opcao) 
-{
-    char coef[3] = {'x', 'y', 'z'}; 
-
-    cout << endl << "Sistema inserido:" << endl;
-
-    for(int i = 0; i < opcao + 1; i++) {
-        for(int j = 0; j <= opcao + 1; j++) {
-            if (matriz[i][j] < 0)
-                cout << "+ " << "(" << matriz[i][j] << coef[j] << ")" << " ";
-            else 
-                cout << "+ " << matriz[i][j] << coef[j] << " ";
-
-
-            if( j == 2) {
-                cout << "= " << matriz[i][3] << endl;
-                break;
-            }
-        }
-    }
-}
-
 int main() {
 
     setlocale(LC_ALL, "");
 
+    char tecla;
     int opcao = 0;
+    float *det, x, y, z;
 
     do {
             system("clear");          
@@ -160,9 +152,9 @@ int main() {
             cout << "  |                                           |" << endl;
             cout << "  |           +====== Menu ======+            |" << endl;
             cout << "  |           |                  |            |" << endl;
-            cout << "  |             [1] 2 Variáveis               |" << endl;
-            cout << "  |             [2] 3 Variáveis               |" << endl;
-            cout << "  |             [0] Sair                      |" << endl;
+            cout << "  |              [1] 2 Variáveis              |" << endl;
+            cout << "  |              [2] 3 Variáveis              |" << endl;
+            cout << "  |              [0] Sair                     |" << endl;
             cout << "  |           |                  |            |" << endl;
             cout << "  |           +------------------+            |" << endl;
             cout << "  |                                           |" << endl;
@@ -172,9 +164,8 @@ int main() {
             //_flushall();
             cin >> opcao;
 
-            float matriz[opcao + 1][4], *det, x, y, z;
-            char tecla;
-            //matriz = (float **) malloc((opcao + 1) * sizeof(float));
+            // Define o tamanho da matriz
+            float matriz[opcao + 1][4];
 
             switch (opcao)
             {
@@ -187,17 +178,16 @@ int main() {
                     x = det[1] / det[0];
                     y = det[2] / det[0];
 
-                    cout << endl << "Determinante: " << det[0] << endl;
-                    cout << "Valor x: " << x << endl;
-                    cout << "Valor y: " << y << endl;
+                    cout << endl;
+                    cout << " +------------ Resultado ------------+" << endl;
+                    cout << "  Determinante: " << det[0] << endl;
+                    cout << "  Valor de x: " << x << endl;
+                    cout << "  Valor de y: " << y << endl;
+                    cout << " +-----------------------------------+" << endl;
 
-                    cout << endl << "Pressione enter para retornar ao menu..." << endl;
-                    if(tecla != '\n') {
-                        int c;
-                        while ((c = getchar()) != '\n' && c != EOF) { }
-                        scanf("%c", &tecla);
-                        break;
-                    }
+                    cout << endl << "Pressione 0 para retornar ao menu..." << endl;
+                    cin >> tecla;
+                    break;
 
                 case 2:
                     system("clear");
@@ -209,19 +199,18 @@ int main() {
                     y = det[2] / det[0];
                     z = det[3] / det[0];
 
-                    cout << endl << "Determinante: " << det[0] << endl;
-                    cout << "Valor x: " << x << endl;
-                    cout << "Valor y: " << y << endl;
-                    cout << "Valor z: " << z << endl;
+                    cout << endl;
+                    cout << " +------------ Resultado ------------+" << endl;
+                    cout << "  Determinante: " << det[0] << endl;
+                    cout << "  Valor de x: " << x << endl;
+                    cout << "  Valor de y: " << y << endl;
+                    cout << "  Valor de z: " << z << endl;
+                    cout << " +-----------------------------------+" << endl;
 
                     cout << endl << "Pressione enter para retornar ao menu..." << endl;
                     
-                    if(tecla != '\n') {
-                        int c;
-                        while ((c = getchar()) != '\n' && c != EOF) { }
-                        scanf("%c", &tecla);
-                        break;
-                    }
+                    cin >> tecla;
+                    break;
                     
                 case 0: 
                     return 0;
@@ -229,6 +218,7 @@ int main() {
     } while(opcao != 0);
 }
 
-// 1 2 1 0
+// Test case:
+// 1 2 1 0 
 // 2 -1 1 1
-// -1 3 1 -2
+// -1  3 1 -2
